dc91378434e328013fb277ea5e21eaa3d32c4e0d
bsmall155 <bsmall@siue.edu>
2023-03-03 21:27:00 -0600

    enter

---
+++ buttons.js
@@ -0,0 +1,3 @@
+cap.fillStyle = "#FF0000";
+cap.fillRect(0, 0, 75, 75);
+
--- colors.js
+++ colors.js
@@ -1,4 +1,3 @@
 var canvas = document.getElementById("Canvas");
 var cap = canvas.getContext("2d");
 cap.fillStyle = "#FF0000";
-cap.fillRect(0, 0, 150, 75);
