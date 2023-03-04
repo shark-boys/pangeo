f8baefe414b27de96b61d0eba832017ffb004a40
bsmall155 <bsmall@siue.edu>
2023-03-04 12:02:02 -0600

    toggle tectonic

--- paint.js
+++ paint.js
@@ -1,17 +1,31 @@
-var tool = 3;
+var tool = 4;
 var isPaint;
 
 function paint(x, y){
-    tiles[y-1][x].setHeight(tool);
-    tiles[y-1][x].setHeight(tool);
-    tiles[y-1][x].setHeight(tool);
-    tiles[y][x-1].setHeight(tool);
-    tiles[y][x].setHeight(tool);
-    tiles[y][x+1].setHeight(tool);
-    tiles[y+1][x].setHeight(tool);
-    tiles[y+1][x].setHeight(tool);
-    tiles[y+1][x].setHeight(tool);
-    draw();
+    if(isTectonic){
+        tiles[y-1][x-1].setDirection(tool);
+        tiles[y-1][x].setDirection(tool);
+        tiles[y-1][x+1].setDirection(tool);
+        tiles[y][x-1].setDirection(tool);
+        tiles[y][x].setDirection(tool);
+        tiles[y][x+1].setDirection(tool);
+        tiles[y+1][x-1].setDirection(tool);
+        tiles[y+1][x].setDirection(tool);
+        tiles[y+1][x+1].setDirection(tool);
+    }
+    else {
+        tiles[y-1][x].setHeight(tool);
+        tiles[y-1][x].setHeight(tool);
+        tiles[y-1][x].setHeight(tool);
+        tiles[y][x-1].setHeight(tool);
+        tiles[y][x].setHeight(tool);
+        tiles[y][x+1].setHeight(tool);
+        tiles[y+1][x].setHeight(tool);
+        tiles[y+1][x].setHeight(tool);
+        tiles[y+1][x].setHeight(tool);
+    }
+    if(isTectonic){drawT();}
+    else {draw();}
 }
 
 function changeTool(t){
