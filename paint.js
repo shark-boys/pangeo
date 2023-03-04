4cd09faa7fcc823dae8ebe07e1b52c6b5bced946
bsmall155 <bsmall@siue.edu>
2023-03-04 01:23:47 -0600

    starting paint function

---
+++ paint.js
@@ -0,0 +1,17 @@
+var tool;
+function paint(x, y, tool){
+    var height = tool;
+    tiles[y][x].setHeight(tool);
+}
+
+function detXtile(x){
+    
+}
+
+function detYtile(y){
+
+}
+
+function changeTool(t){
+    tool = t;
+}
