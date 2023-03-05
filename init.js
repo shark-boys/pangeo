062a029db979b7239afd7f2e6d911ac63ff865b8
loglug1 <62907417+loglug1@users.noreply.github.com>
2023-03-04 21:42:45 -0600

    changed init to single direction

--- init.js
+++ init.js
@@ -16,20 +16,10 @@ function initilize(){
     for (y = 0; y < gridSize; y++) {
         for (x = 0; x < gridSize; x++) {
             tiles[y][x] = new Tile(-1, 0, false,0,false, x, y);
+            tiles[y][x].weight = Math.floor(Math.random() * 10);
         }
     }
 
-    for (i = 0; i < 2; i++) {
-        direction = 2; //Math.floor(Math.random() * 8);
-        fraction = gridSize / 2;
-        weight = Math.floor(Math.random() * 10);
-        for (y = i * fraction; y < ((i + 1) * fraction); y++) {
-            for (x = i * fraction; x < ((i + 1) * fraction); x++) {
-                tiles[y][x].direction = direction;
-                tiles[y][x].weight = weight;
-            }
-        }
-    }
     draw();
     resizeCanvas();
 }
