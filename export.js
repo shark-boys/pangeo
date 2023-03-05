f2244b2678f23066ff24924867cb7fdd701db3d2
loglug1 <62907417+loglug1@users.noreply.github.com>
2023-03-05 01:57:02 -0600

    added preset imports

--- export.js
+++ export.js
@@ -10,3 +10,22 @@ function importString(str) {
         draw();
     }
 }
+
+function importPreset(num) {
+    importString(presets[num]);
+}
+
+
+/*
+    KEY:
+    0 = two stacked all land
+    1 = two stacked all water
+    2 = four quadrants moving together island
+    3 = four quadrants moving apart island
+*/
+const presets = [
+    '',
+    '',
+    '',
+    ''
+]
