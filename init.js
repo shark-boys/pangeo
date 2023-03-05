59ffe64cfc732773f864c3046ffae8ec5ec56207
Caleb Sutton <115527079+C4LSUTT@users.noreply.github.com>
2023-03-05 02:24:27 -0600

    start w water

--- init.js
+++ init.js
@@ -20,7 +20,7 @@ function initilize(){
             direction = 0;
         }
         for (x = 0; x < gridSize; x++) {
-            tiles[y][x] = new Tile(1, direction, false,0,false, x, y);
+            tiles[y][x] = new Tile(-1, direction, false,0,false, x, y);
             tiles[y][x].weight = Math.floor(Math.random() * 10);
             lavaBool = Math.floor(Math.random()*100)
             if (lavaBool == 1){
